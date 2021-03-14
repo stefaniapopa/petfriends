af321276a9cf2c43825d0d5af1f07b7de2432b6c
Stefania Popa <stefania_florina10@yahoo.com>
2021-03-14 20:34:58 +0200

    did petfriends app

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 import './App.css';
 
 
@@ -26,13 +26,13 @@ class App extends Component {
     }
 
     render() {
-        const filteredPets = this.state.pets.filter(pet => {
-            return pet.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const { pets, searchfield } = this.state;
+        const filteredPets = pets.filter(pet => {
+            return pet.name.toLowerCase().includes(searchfield.toLowerCase());
         })
-        if (this.state.pets.length === 0) {
-            return <h1>Loading</h1>
-        } else {
-            return (
+        return !pets.length ?
+            <h1>Loading</h1> :
+            (
                 <div className='tc'>
                     <h1 className='f1'>PetFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
@@ -41,7 +41,7 @@ class App extends Component {
                     </Scroll>
                 </div>
             );
-        }
+
 
     }
 
